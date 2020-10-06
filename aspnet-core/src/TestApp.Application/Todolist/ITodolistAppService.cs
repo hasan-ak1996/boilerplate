d0e144e0d81b25b41cd60ddb4e5a7ef09ac01037
mhd hasan alhallak <hasan_ak1996@hotmail.com>
2020-10-06 21:08:57 +0300

    secound

---
+++ aspnet-core/src/TestApp.Application/Todolist/ITodolistAppService.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Todo_list;
+
+namespace TestApp.Todolist
+{
+    public interface ITodolistAppService : IApplicationService
+    {
+        void CreateTask(TodoListDto input);
+        TodoList GetTaskById(TodoListDto input);
+        IEnumerable<TodoList> GetAllTasks();
+        void UpdateTask(TodoListDto input);
+        void DeleteTask(TodoListDto input);
+    }
+}
--- aspnet-core/src/TestApp.Application/Todolist/TodolistAppService.cs
+++ aspnet-core/src/TestApp.Application/Todolist/TodolistAppService.cs
@@ -2,6 +2,8 @@
 using Abp.Application.Services.Dto;
 using Abp.Authorization;
 using Abp.Domain.Repositories;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -13,12 +15,47 @@ using TestApp.Todo_list;
 
 namespace TestApp.Todolist
 {
+    
     [AbpAuthorize(PermissionNames.Pages_Todolist)]
-    public class TodolistAppService : CrudAppService<TodoList, TodoListDto>
+    public class TodolistAppService : ApplicationService, ITodolistAppService
     {
-        public TodolistAppService(IRepository<TodoList, int> repository) : base(repository)
+        private readonly IRepository<TodoList> repository;
+
+        public TodolistAppService(IRepository<TodoList> repository)
         {
+            this.repository = repository;
         }
+
+        public void UpdateTask(TodoListDto input)
+        {
+            var newTask = repository.Get(input.Id);
+            repository.Update(newTask);
+        }
+        public void DeleteTask(TodoListDto input)
+        {
+            var task = repository.Get(input.Id);
+            repository.Delete(task);
+        }
+
+        public void CreateTask(TodoListDto input)
+        {
+            var task = new TodoList { Id = input.Id, title = input.title };
+            repository.Insert(task);
+        }
+
+        public TodoList GetTaskById(TodoListDto input)
+        {
+            var task = repository.Get(input.Id);
+            return task;
+        }
+
+        public IEnumerable<TodoList> GetAllTasks()
+        {
+            List<TodoList> tasks = repository.GetAllList();
+            return tasks;
+        }
+
+
 
     }
 }
