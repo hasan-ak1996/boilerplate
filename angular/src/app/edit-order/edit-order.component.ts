ba7922b6b5c66640b29ff2ff3fc4d0829fc6c5fc
mhd hasan alhallak <hasan_ak1996@hotmail.com>
2020-10-14 00:44:20 +0300

    m

--- angular/src/app/edit-order/edit-order.component.ts
+++ angular/src/app/edit-order/edit-order.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Injector, OnInit, Output } from '@angular/core';
+import { AppComponentBase } from '@shared/app-component-base';
+import { GetOrederOutputDTO, OrderServiceProxy } from '@shared/service-proxies/service-proxies';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-order',
   templateUrl: './edit-order.component.html',
   styleUrls: ['./edit-order.component.css']
 })
-export class EditOrderComponent implements OnInit {
+export class EditOrderComponent extends AppComponentBase  implements OnInit {
+  saving = false;
+  order = new GetOrederOutputDTO ();
+  id: number;
+  @Output() onSave = new EventEmitter<any>();
 
-  constructor() { }
+  constructor(
+    injector: Injector,
+    public _OrderService: OrderServiceProxy,
+    public bsModalRef: BsModalRef
+  ) { 
+    super(injector);
+  }
 
   ngOnInit(): void {
+    this._OrderService.getOrderById(this.id).subscribe((result) => {
+      this.order = result; 
+       })
+  }
+  save(): void {
+    this.saving = true;
+
+
+    this._OrderService
+      .updateOrder(this.order)
+      .pipe(
+        finalize(() => {
+          this.saving = false;
+        })
+      )
+      .subscribe(() => {
+        this.notify.info(this.l('SavedSuccessfully'));
+        this.bsModalRef.hide();
+        this.onSave.emit();
+      });
+
   }
 
 }
--- angular/src/app/view-orders/view-orders.component.ts
+++ angular/src/app/view-orders/view-orders.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Injector, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { EditOrderComponent } from '@app/edit-order/edit-order.component';
 import { AppComponentBase } from '@shared/app-component-base';
 import {
   OrderServiceProxy,
   GetOrederOutputDTO,
   
 } from '@shared/service-proxies/service-proxies';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-view-orders',
   templateUrl: './view-orders.component.html',
@@ -15,7 +17,8 @@ export class ViewOrdersComponent  extends AppComponentBase implements OnInit {
   orders : GetOrederOutputDTO[] =[];
   @Output() onSave = new EventEmitter<any>();
   constructor(injector: Injector ,
-    public _orderService: OrderServiceProxy, ) {
+    public _orderService: OrderServiceProxy,
+    private _modalService: BsModalService, ) {
     super(injector);
    }
 
@@ -23,6 +26,30 @@ export class ViewOrdersComponent  extends AppComponentBase implements OnInit {
     this._orderService.getAllOrders().subscribe((result) => {
       this.orders = result;
     });
+
+  }
+
+  editOrder(order: GetOrederOutputDTO): void {
+    this.showEditOrderDialog(order.id);
+  }
+
+  private showEditOrderDialog(id: number): void {
+    let EditOrderDialog: BsModalRef;
+      EditOrderDialog = this._modalService.show(
+        EditOrderComponent,
+        {
+          class: 'modal-lg',
+          initialState: {
+            id: id,
+          },
+        }
+      );
+    
+      EditOrderDialog.content.onSave.subscribe(() => {
+      this._orderService.getAllOrders().subscribe((result) => {
+        this.orders = result;})
+    });
+    
 
   }
 
